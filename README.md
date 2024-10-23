1ea1891812b23406d062647b03dca0a7382a7c13
ShiKoLatA <sabouela@lakeheadu.ca>
2024-10-23 23:49:03 +0300

    feat: added readme

---
+++ README.md
@@ -0,0 +1,45 @@
+# PrismsEventConsumer
+
+The application is an event consumer, which is intended to receive events from the message queue, and perform the following steps:
+1. Send event info to long-term storage.
+2. Check the cache for if a score exists (StudentHealth) for the event's student.
+   a. If so, use the existing data to calculate the new score, and skip to step 6.
+   b. If not, This is the first event for that student for that class; proceed through to calculate the new score based on just the new event.
+3.  Hydrate StudentHealth details.
+4.  Fetch score calculation details.
+5.  Calculate new StudentHealth score.
+6.  Update the score in redis.
+7.  Send request to db/cache to store StudentHealth.
+
+## Running Structure Bikes locally
+
+Run `npm i` to install all the packages.
+Run `npm start` will start the dev server.
+
+Use postman to initiate an initial request as the following:
+- Method: POST
+- Body:
+
+```
+{
+    "userId": 1,
+    "applicationId": 1,
+    "institutionId": 1,
+    "classId": 1,
+    "taskId": 1,
+    "actionId": 1,
+    "timeStamp": "timeStamp"
+}
+```
+
+## Requirements
+
+This application requires setting up redis on your local environment.
+Follow the steps in the following document depending on your environment: https://redis.io/docs/latest/operate/oss_and_stack/install/install-redis/
+
+## In Progress / Nice to haves
+
+- Calculation Service
+- Unit Testing
+- Batch Event Processing
+- Swagger
--- src/routes/trackingRouter.ts
+++ src/routes/trackingRouter.ts
@@ -13,24 +13,24 @@ trackingRouter.post('/', async (req: Request, res: Response) => {
     try {
         const event: VrEvent = req.body;
 
-        // 1. send event info to long-term storage
+        // 1. Send event info to long-term storage.
         await postEventToEventStore(event);
 
+        // 2. Check the cache for if a score exists (StudentHealth) for the event's student.
         let studentHealth: StudentHealth = await getCacheValue(event.userId.toString());
 
         if (!!studentHealth && studentHealth.classId == event.classId) {
-            // use the existing data along with the new event to calculate the new score
+            // 2a. If so, use the existing data to calculate the new score, and skip to step 6.
             studentHealth.completedActions++;
             studentHealth.score = calculateStudentScore(studentHealth.completedActions, studentHealth.totalActions);
         } else {
-            // This is the first event for that student for that class
-            // calculate the new score based on just the new event
+            // 2b. This is the first event for that student for that class; proceed through to calculate the new score based on just the new event.
             studentHealth = {
                 userId: event.userId,
                 classId: event.classId
             }
 
-            // 2. Hydration of StudentHealth details
+            // 3. Hydrate StudentHealth details.
             const vrUser: VrUser = await fetchUser(studentHealth.userId);
             studentHealth.firstName = vrUser.firstName;
             studentHealth.lastName = vrUser.lastName; 
@@ -38,20 +38,21 @@ trackingRouter.post('/', async (req: Request, res: Response) => {
             const vrClass: VrClass = await fetchClass(studentHealth.classId);
             studentHealth.className = vrClass.className;
 
-            // 3. Calculate new StudentHealth score
+            // 4. Fetch score calculation details.
             const classTasks: number[] = await fetchClassTasks(studentHealth.classId);
 
             const totalActions: number = await fetchTotalActions(classTasks);
 
+            // 5. Calculate new StudentHealth score.
             studentHealth.completedActions = 1;
             studentHealth.totalActions = totalActions;
             studentHealth.score = calculateStudentScore(studentHealth.completedActions, studentHealth.totalActions);
         }
 
-        // 4. Update the score in redis
+        // 6. Update the score in redis.
         await setCacheValue(String(studentHealth.userId), JSON.stringify(studentHealth));
 
-        // 5. send request to db/cache to store student health
+        // 7. Send request to db/cache to store StudentHealth.
         await postStudentHealthToDashboardStore(studentHealth);
 
         res.sendStatus(200);
